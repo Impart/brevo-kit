d64e4853cdf79c4f63ae5f410185c079139e5a8c
Marcel Borsten <marcel@impart.nl>
2024-02-21 12:15:19 +0100

    Update BrevoTemplateEmail.swift

--- Sources/BrevoKit/Models/BrevoTemplateEmail.swift
+++ Sources/BrevoKit/Models/BrevoTemplateEmail.swift
@@ -12,10 +12,21 @@ public struct BrevoTemplateEmail: Codable {
     public struct Recipient: Codable {
         let email: String
         let name: String
+
+        public init(email: String, name: String) {
+            self.email = email
+            self.name = name
+        }
     }
     
     let to: [Recipient]
     let templateId: Int
     let params: [String: String]
+
+    public init(to: [Recipient], templateId: Int, params: [String, String]) {
+        self.to = to
+        self.templateId = templateId
+        self.params = params
+    }
     
 }
